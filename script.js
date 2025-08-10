17261976430074b9990f1a85fc53791f0bc31e75
Austriana <Marco.Mang@gmx.de>
2025-08-11 00:02:55 +0200

    ...

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ let fetch_data = async () => {
       throw new Error(`Response status: ${response.status}`);
     }
     const result = await response.json();
-    console.log(result.PASSWORD)
+    console.log(convertData(result));
   } catch (error) {
     console.error(error.message);
   }
+}
+const convertData = result => {
+    const name = result.Name;
+    const email = result.Email;
+    const adresse = result.Adresse;
+    const telefon = result.Telefon;
+    const passwort = result.PASSWORD;
+    return name, adresse, email, telefon, passwort;
 }
 
 fetch_data();
