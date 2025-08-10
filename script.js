6bbff48a8751e68d512717e482eb8fe65ea42592
Austriana <Marco.Mang@gmx.de>
2025-08-10 21:48:22 +0200

    ...

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let fetch_data =  async (res, e) => {
-    let response = await fetch('https://austriana.github.io/kontakt/data.json')).json;
+let fetch_data =  async () => {
+    let response = await fetch('https://austriana.github.io/kontakt/data.json').json;
     return response;
 }
-let obj = fetch_data();
